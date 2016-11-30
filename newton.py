665b5bdead4431e5049d9fbb393c9e430e923ca2
Jeff Lestz <jlestz@pppl.gov>
2016-11-30 00:51:26 -0500

    added search radius feature to newton and relevant tests to testNewton. Left to do: add analytic Jacobian feature and tests and expand the README

--- newton.py
+++ newton.py
@@ -25,16 +25,16 @@ class Newton(object):
         x = x0
         r = self._r
         for i in xrange(self._maxiter+1):
+            fx = self._f(x)
             # if |x - x0| > r, throw an exception 
             if N.linalg.norm(x - x0) > r: 
                 raise RuntimeError("Distance x - x0 exceeds search radius r")
-            fx = self._f(x)
             if N.linalg.norm(fx) < self._tol:
                 return x
             # if i = maxiter, then maxiter steps have already been taken
             # and no solutions have been found. Raise an error. 
             elif i == self._maxiter: 
-                raise RuntimeError("No solution found after max_iter iterations")
+                raise RuntimeError("No solution found after maxiter iterations")
             x = self.step(x, fx)
 
     def step(self, x, fx=None):
--- testNewton.py
+++ testNewton.py
@@ -46,7 +46,41 @@ class TestNewton(unittest.TestCase):
             pass 
         else: 
             self.fail('No error raised'); 
-        
+
+    # test if maxiters exception is raised at correct step number 
+    # (e.g. make sure loop is robust to edge cases of solutions that are found exactly 1 iteration after maxiters) 
+    # with tol=2^-5, solution requires 3 iterations
+    # maxiter = 2 should raise an error 
+    def testMaxItersReached(self): 
+        f = lambda x: x**2
+        solver = newton.Newton(f,tol=2**-5,maxiter=2)
+        x0=1.0 
+        try: 
+            x = solver.solve(x0) 
+        except RuntimeError as err: 
+            if "maxiter" in err.message: 
+                pass 
+            else: 
+                self.fail('Wrong error raised')
+        else: 
+            self.fail('No error raised')
+    
+    # test if maxiters exception is raised at correct step number 
+    # (e.g. make sure loop is robust to edge cases of solutions that are found exactly 1 iteration after maxiters) 
+    # with tol=2^-5, solution requires 3 iterations 
+    # maxiter = 3 should not raise an error 
+    def testMaxItersNotReached(self): 
+        f = lambda x: x**2
+        solver = newton.Newton(f,tol=2**-5,maxiter=3)
+        x0=1.0 
+        try: 
+            x = solver.solve(x0) 
+        except RuntimeError as err: 
+            if "maxiter" in err.message: 
+                self.fail('Error raised erroneously')
+        else: 
+            self.assertAlmostEqual(x,2**-3,places=6)
+
     # test that the correct step is taken in a known case 
     def testStep(self): 
         f = lambda x : x + 1.0
@@ -63,7 +97,7 @@ class TestNewton(unittest.TestCase):
         except RuntimeError: 
             pass 
         else: 
-            self.fail('No error raised'); 
+            self.fail('No error raised')
 
     # test for a constant function (no root, singular Jacobian)
     # should throw exception since singular Jacobian breaks method
@@ -75,9 +109,66 @@ class TestNewton(unittest.TestCase):
         except N.linalg.LinAlgError: 
             pass 
         else: 
-            self.fail('No error raised'); 
+            self.fail('No error raised')
 
-    # verify 
+    # verify f(x) = e^x fails due to maxiters (within search radius)
+    # for f(x) = e^x, x_{k+1} = x_k - 1
+    # maxiters = 3 should fail before r = 4 for x0 = 0
+    def testExpMaxIters(self): 
+        f = lambda x: N.exp(x)
+        solver = newton.Newton(f,tol=1.e-15,maxiter=3,r=4) 
+        x0=0.0
+        try: 
+            x = solver.solve(x0) 
+        except RuntimeError as err: 
+            if "maxiter" in err.message: 
+                pass 
+            else: 
+                self.fail('Wrong error raised')
+        else: 
+            self.fail('No error raised')
+
+    # verify f(x) = e^x fails due to search radius (within maxiters)
+    # for f(x) = e^x, x_{k+1} = x_k - 1
+    # maxiters = 4 should not fail before r = 3 for x0 = 0
+    def testExpMaxRadius(self): 
+        f = lambda x: N.exp(x)
+        solver = newton.Newton(f,tol=1.e-15,maxiter=4,r=3) 
+        x0=0.0
+        try: 
+            x = solver.solve(x0) 
+        except RuntimeError as err: 
+            if "radius r" in err.message: 
+                pass 
+            else: 
+                self.fail('Wrong error raised')
+        else: 
+            self.fail('No error raised')
+
+    # test if radius exception is thrown when some intermediate steps leave the search radius even if the roots are known to be within search radius. f(x) = x^2 - 1 has roots at x = +/- 1, but an initial guess of x0=0.1 leads to a first step of x0 near 5, clearly beyond a radius of 2, despite the fact that subsequent steps would lead to convergence at x = 1
+    def testStepBeyondSearch(self): 
+        f = lambda x: x**2 - 1 
+        solver = newton.Newton(f,r=2,maxiter=10) 
+        x0=0.1 
+        try: 
+            x = solver.solve(x0) 
+        except RuntimeError: 
+            pass 
+        else: 
+            self.fail('No error raised') 
+
+    # same test as above except r = 5 to accomodate the first step 
+    # since the first step has the largest error, the solver will converge on the correct solution 
+    def testStepBeyondSearchSuccess(self): 
+        f = lambda x: x**2 - 1 
+        solver = newton.Newton(f,r=5,maxiter=10) 
+        x0=0.1 
+        try: 
+            x = solver.solve(x0) 
+        except RuntimeError: 
+            self.fail('Error raised erroneously')
+        else: 
+            self.assertAlmostEqual(x,1.0)
 
 if __name__ == "__main__":
     unittest.main()
